c0db7e78575bb17dbe219518149f37238186a300
hayderRm <140641607+hayderRm@users.noreply.github.com>
2023-07-29 11:15:48 -0400

    Update DemoDay3.java

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -6,11 +6,11 @@ public class DemoDay3 {
 		System.out.println("Hayder...");
 		
 		System.out.println("Push Hayder to RemoteRepository");
+				System.out.println("pull Hayder to locallRepository");
+
 	}
 
 	String country = "USA";
 	String state = "MA";
-		System.out.println("pull Hayder to locallRepository");
-
 	
 }
