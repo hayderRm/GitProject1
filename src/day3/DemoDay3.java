b32615e46ef8480976bdb8f15c94591a0eacf419
hayde <hayde@10.0.0.28>
2023-07-29 11:13:44 -0400

    my name again

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -4,9 +4,12 @@ public class DemoDay3 {
 
 	public static void main(String[] args) {
 		System.out.println("Hayder...");
+		
+		System.out.println("Push Hayder to RemoteRepository");
 	}
 
-	String country="USA";
-	String state="MA";
+	String country = "USA";
+	String state = "MA";
 
+	
 }
